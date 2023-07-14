0acdc5eb406ea928e6ffd2ac7dbc8d37ca6cdaae
Wenhong Huang <wenhong.huang@miu.edu>
2023-07-14 12:24:06 -0500

    sign in toast

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -1,10 +1,12 @@
 import { CanActivateFn, Router } from "@angular/router";
 import { inject } from "@angular/core";
 import { AuthService } from "./auth.service";
+import { ToastService } from "../toast.service";
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   if (!authService.isSignedIn) {
+    inject(ToastService).showNotification("You must be signed in.");
     inject(Router).navigate(["", "signin"]);
     return false;
   }
